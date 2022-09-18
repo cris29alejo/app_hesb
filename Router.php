d5b2672c7989f7c91c46df0e6c80343e5bd51e09
cris29alejo <cris29alejo@gmail.com>
2022-09-17 19:23:00 -0500

    Subida Version1 App

--- Router.php
+++ Router.php
@@ -28,8 +28,18 @@ class Router
 
         // $auth = $_SESSION['login'] ?? null;
 
-        $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI']; //PRODUCCION'
+        //$currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI']; //PRODUCCION'
         //$currentUrl = $_SERVER['PATH_INFO']  ?? '/';
+
+        
+        if (isset($_SERVER['PATH_INFO'])) {
+            $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        } else {
+            $currentUrl = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
+        }
+
+
+
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET') {
