c335ad3fb8f2378c7d4f78ff79c860875cf43de3
cris29alejo <cris29alejo@gmail.com>
2023-02-27 23:25:33 -0500

    Subida de app

--- Classes/Email.php
+++ Classes/Email.php
@@ -25,8 +25,8 @@ class Email{
         $mail->Host = 'smtp.mailtrap.io';
         $mail->SMTPAuth = true;
         $mail->Port = 2525;
-        $mail->Username = 'a0f641eddc0d16';
-        $mail->Password = 'c6bdb7733c6ba9';
+        $mail->Username = 'e90047fbf5a214';
+        $mail->Password = '00088313534389';
 
         $mail->setFrom('cris29alejo@gmail.com');
         $mail->addAddress($this->email, 'https://murmuring-sea-87576.herokuapp.com');
@@ -36,7 +36,8 @@ class Email{
 
         $contenido = "<html>";
         $contenido .= "<p><strong>Hola ".$this->nombre . "</strong>, has creado tu cuenta en APP Hospital de Especialidades San Bartolo, solo debes cofirmarla presionando el siguiente enlace</p>";
-        $contenido .= "Presionando aquí: <a href='https://murmuring-sea-87576.herokuapp.com/confirmar-cuenta?token=" . $this->token . "'>Confimar Cuenta</a></p>";
+        //$contenido .= "Presionando aquí: <a href='https://murmuring-sea-87576.herokuapp.com/confirmar-cuenta?token=" . $this->token . "'>Confimar Cuenta</a></p>";
+        $contenido .= "Presionando aquí: <a href='http:localhost:3000/confirmar-cuenta?token=" . $this->token . "'>Confimar Cuenta</a></p>";
         $contenido .="<p>Si tu no solicitaste esta cuenta, puedes ignorar el mensaje</p>";
         $contenido .= "</html>";
 
@@ -51,8 +52,8 @@ class Email{
         $mail->Host = 'smtp.mailtrap.io';
         $mail->SMTPAuth = true;
         $mail->Port = 2525;
-        $mail->Username = 'a0f641eddc0d16';
-        $mail->Password = 'c6bdb7733c6ba9';
+        $mail->Username = 'e90047fbf5a214';
+        $mail->Password = '00088313534389';
 
         $mail->setFrom('cris29alejo@gmail.com');
         $mail->addAddress($this->email, 'https://murmuring-sea-87576.herokuapp.com');
@@ -62,7 +63,8 @@ class Email{
 
         $contenido = "<html>";
         $contenido .= "<p><strong>Hola ".$this->nombre . "</strong>, has solicitado restablecer tu contraseña,sigue el sigueinte enlace</p>";
-        $contenido .= "Presionando aquí: <a href='https://murmuring-sea-87576.herokuapp.com/recuperar?token=" . $this->token . "'>Restablecer Contraseña</a></p>";
+        //$contenido .= "Presionando aquí: <a href='https://murmuring-sea-87576.herokuapp.com/recuperar?token=" . $this->token . "'>Restablecer Contraseña</a></p>";
+        $contenido .= "Presionando aquí: <a href='http:localhost:3000/recuperar?token=" . $this->token . "'>Restablecer Contraseña</a></p>";
         $contenido .="<p>Si tu no solicitaste este cambio, puedes ignorar el mensaje</p>";
         $contenido .= "</html>";
 
--- views/index.php
+++ views/index.php
@@ -4,7 +4,11 @@
     </div>
     <div class="contenedor">
         <h3>Hospital de Especialidades San Bartolo</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam debitis fuga odio mollitia fugiat doloribus odit nulla amet esse repellat, aliquam at, ex aut. Iure laudantium eaque harum magnam expedita.</p>
+        <ul><li><p>El Hospital de Especialidades San Bartolo es un centro médico privado que brinda atención médica especializada en diversas áreas de la salud.</li></ul>
+        <ul><li><p>Ofrece servicios de alta complejidad, como cirugía cardiovascular, neurología, oncología, traumatología, entre otros.</li></ul>
+        <ul><li><p>Cuenta con equipos y tecnología de última generación para realizar diagnósticos precisos y tratamientos efectivos.</li></ul>
+        <ul><li><p>El hospital está conformado por un equipo de profesionales altamente capacitados en sus respectivas especialidades.</li></ul>
+        
         <a href="#" class="btn"> Contacto<span class="fas fa-chevron-right"></span></a>
     </div>
     
